c5a9d4267e5f496e854a42ac1e57d05976e44fbc
Sxmurai <gavrw0607@gmail.com>
2021-09-10 15:45:21 -0500

    -5 iq lmao i forgot to use hotbar > all inventory slots

--- src/main/java/me/sxmurai/inferno/features/modules/miscellaneous/MiddleClick.java
+++ src/main/java/me/sxmurai/inferno/features/modules/miscellaneous/MiddleClick.java
@@ -2,6 +2,7 @@ package me.sxmurai.inferno.features.modules.miscellaneous;
 
 import me.sxmurai.inferno.Inferno;
 import me.sxmurai.inferno.features.settings.Setting;
+import me.sxmurai.inferno.managers.commands.Command;
 import me.sxmurai.inferno.managers.modules.Module;
 import me.sxmurai.inferno.utils.InventoryUtils;
 import net.minecraft.entity.player.EntityPlayer;
@@ -16,50 +17,51 @@ import org.lwjgl.input.Mouse;
 public class MiddleClick extends Module {
     public final Setting<OverPlayer> overPlayer = this.register(new Setting<>("OverPlayer", OverPlayer.NOTHING));
     public final Setting<Boolean> unfriend = this.register(new Setting<>("Unfriend", true, (v) -> overPlayer.getValue() == OverPlayer.FRIEND));
+    public final Setting<Boolean> notify = this.register(new Setting<>("Notify", true, (v) -> overPlayer.getValue() == OverPlayer.FRIEND));
     public final Setting<Boolean> pearl = this.register(new Setting<>("Pearl", false));
     public final Setting<Boolean> offhand = this.register(new Setting<>("Offhand", true, (v) -> pearl.getValue()));
     public final Setting<Boolean> silentSwitch = this.register(new Setting<>("SilentSwitch", false, (v) -> pearl.getValue()));
 
     @SubscribeEvent
-    public void onMouseInput(InputEvent.KeyInputEvent event) {
-        if (!Module.fullNullCheck() && !Mouse.getEventButtonState()) {
-            if (mc.objectMouseOver.typeOfHit == RayTraceResult.Type.MISS) {
-                if (pearl.getValue()) {
-                    int slot = InventoryUtils.getInventoryItemSlot(Items.ENDER_PEARL, offhand.getValue());
-                    if (slot == -1) {
+    public void onMouseInput(InputEvent.MouseInputEvent event) {
+        if (!Module.fullNullCheck() && Mouse.getEventButton() == 2 && !Mouse.getEventButtonState() && mc.objectMouseOver != null) {
+            if (mc.objectMouseOver.typeOfHit == RayTraceResult.Type.ENTITY && mc.objectMouseOver.entityHit instanceof EntityPlayer) {
+                EntityPlayer player = (EntityPlayer) mc.objectMouseOver.entityHit;
+
+                if (Inferno.friendManager.isFriend(player)) {
+                    if (!unfriend.getValue()) {
                         return;
                     }
 
-                    int oldSlot = mc.player.inventory.currentItem;
-                    if (slot != 45) {
-                        InventoryUtils.switchTo(slot, silentSwitch.getValue());
+                    Inferno.friendManager.removeFriend(player);
+                    if (notify.getValue()) {
+                        Command.send("Unfriended " + player.getName());
                     }
-
-                    mc.playerController.processRightClick(mc.player, mc.world, slot == 45 ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND);
-
-                    if (slot != 45) {
-                        InventoryUtils.switchTo(oldSlot, silentSwitch.getValue());
+                } else {
+                    Inferno.friendManager.addFriend(player);
+                    if (notify.getValue()) {
+                        Command.send("Friended " + player.getName());
                     }
                 }
-            } else if (mc.objectMouseOver.typeOfHit == RayTraceResult.Type.ENTITY && mc.objectMouseOver.entityHit instanceof EntityPlayer) {
-                if (overPlayer.getValue() == OverPlayer.NOTHING) {
+
+                return;
+            }
+
+            if (pearl.getValue()) {
+                int slot = InventoryUtils.getHotbarItemSlot(Items.ENDER_PEARL, offhand.getValue());
+                if (slot == -1) {
                     return;
                 }
 
-                EntityPlayer player = (EntityPlayer) mc.objectMouseOver.entityHit;
+                int oldSlot = mc.player.inventory.currentItem;
+                if (slot != 45) {
+                    InventoryUtils.switchTo(slot, silentSwitch.getValue());
+                }
 
-                if (overPlayer.getValue() == OverPlayer.FRIEND) {
-                    if (Inferno.friendManager.isFriend(player)) {
-                        if (!unfriend.getValue()) {
-                            return;
-                        }
+                mc.playerController.processRightClick(mc.player, mc.world, slot == 45 ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND);
 
-                        Inferno.friendManager.removeFriend(player);
-                    } else {
-                        Inferno.friendManager.addFriend(player);
-                    }
-                } else if (overPlayer.getValue() == OverPlayer.DUEL) {
-                    mc.player.sendChatMessage("/duel " + player.getName());
+                if (slot != 45) {
+                    InventoryUtils.switchTo(oldSlot, silentSwitch.getValue());
                 }
             }
         }
