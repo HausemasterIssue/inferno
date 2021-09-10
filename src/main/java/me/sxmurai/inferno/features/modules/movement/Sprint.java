032bd932d55416bcc05367c42078220fc4333634
Sxmurai <gavrw0607@gmail.com>
2021-09-10 15:20:12 -0500

    add hunger check to sprint

--- src/main/java/me/sxmurai/inferno/features/modules/movement/Sprint.java
+++ src/main/java/me/sxmurai/inferno/features/modules/movement/Sprint.java
@@ -3,17 +3,17 @@ package me.sxmurai.inferno.features.modules.movement;
 import me.sxmurai.inferno.events.mc.UpdateEvent;
 import me.sxmurai.inferno.features.settings.Setting;
 import me.sxmurai.inferno.managers.modules.Module;
-import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module.Define(name = "Sprint", description = "Makes you automatically sprint", category = Module.Category.MOVEMENT)
 public class Sprint extends Module {
     public final Setting<Mode> mode = this.register(new Setting<>("Mode", Mode.LEGIT));
+    public final Setting<Boolean> hungerCheck = this.register(new Setting<>("HungerCheck", false));
 
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
-        if (mode.getValue() == Mode.LEGIT && mc.gameSettings.keyBindForward.pressed || mode.getValue() == Mode.RAGE) {
-            KeyBinding.setKeyBindState(mc.gameSettings.keyBindSprint.keyCode, true);
+        if ((mode.getValue() == Mode.LEGIT && !mc.gameSettings.keyBindForward.pressed || mode.getValue() == Mode.RAGE) && (mc.player.getFoodStats().getFoodLevel() <= 6 && hungerCheck.getValue())) {
+            mc.gameSettings.keyBindSprint.pressed = true;
         }
     }
 
