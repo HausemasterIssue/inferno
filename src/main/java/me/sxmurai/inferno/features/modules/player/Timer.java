f340ab479b82c06ca6b41c8608f4b2814435546b
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-09-11 15:24:07 -0400

    tps sync in timer

--- src/main/java/me/sxmurai/inferno/features/modules/player/Timer.java
+++ src/main/java/me/sxmurai/inferno/features/modules/player/Timer.java
@@ -7,13 +7,30 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module.Define(name = "Timer", description = "Makes the game go zoom zoom", category = Module.Category.PLAYER)
 public class Timer extends Module {
-    public final Setting<Float> speed = this.register(new Setting<>("Speed", 2.0f, 0.1f, 20.0f));
+    public final Setting<Float> speed = this.register(new Setting<>("ClientTPS", 2.0f, 0.1f, 20.0f));
+    public final Setting<Boolean> sync = this.register(new Setting<>("TPS Sync", true));
 
     @Override
     protected void onActivated() {
         if (Module.fullNullCheck()) {
             toggle();
         }
+        
+        public static double getTps() {
+        int tickCount = 0;
+        float tickRate = 0.0f;
+
+        for (int i = 0; i < ticks.length; i++) {
+            final float tick = ticks[i];
+
+            if (tick > 0.0f) {
+                tickRate += tick;
+                tickCount++;
+            }
+        }
+
+        return MathHelper.clamp((tickRate / tickCount), 0.0f, 20.0f);
+	}
     }
 
     @Override
@@ -25,6 +42,12 @@ public class Timer extends Module {
 
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
-        mc.timer.tickLength = 50.0f / speed.getValue();
+        
+       if (sync.booleanValue()) {
+           mc.timer.tickLength = 50.0f / getTps());
+       } else {
+         mc.timer.tickLength = 50.0f / speed.getValue();
+       }
+
     }
 }
