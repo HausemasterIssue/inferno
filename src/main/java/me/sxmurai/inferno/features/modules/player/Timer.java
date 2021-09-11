161fb7f8eab99997871809788ca1bc536b9d8b17
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-09-11 15:35:22 -0400

    for real this time, added tps sync
    accept it this time i tested it and it should work

--- src/main/java/me/sxmurai/inferno/features/modules/player/Timer.java
+++ src/main/java/me/sxmurai/inferno/features/modules/player/Timer.java
@@ -3,11 +3,31 @@ package me.sxmurai.inferno.features.modules.player;
 import me.sxmurai.inferno.events.mc.UpdateEvent;
 import me.sxmurai.inferno.features.settings.Setting;
 import me.sxmurai.inferno.managers.modules.Module;
+import net.minecraft.util.math.MathHelper;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module.Define(name = "Timer", description = "Makes the game go zoom zoom", category = Module.Category.PLAYER)
 public class Timer extends Module {
-    public final Setting<Float> speed = this.register(new Setting<>("Speed", 2.0f, 0.1f, 20.0f));
+    public final Setting<Float> speed = this.register(new Setting<>("ClientTPS", 2.0f, 0.1f, 20.0f));
+    public final Setting<Boolean> sync = this.register(new Setting<>("TPS Sync", true));
+    
+    private static float[] ticks = new float[20];
+    
+    public static double getTps() {
+        int tickCount = 0;
+        float tickRate = 0.0f;
+
+        for (int i = 0; i < ticks.length; i++) {
+            final float tick = ticks[i];
+
+            if (tick > 0.0f) {
+                tickRate += tick;
+                tickCount++;
+            }
+        }
+
+        return MathHelper.clamp((tickRate / tickCount), 0.0f, 20.0f);
+	}
 
     @Override
     protected void onActivated() {
@@ -25,6 +45,12 @@ public class Timer extends Module {
 
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
-        mc.timer.tickLength = 50.0f / speed.getValue();
+    	if(sync.getValue() == true) {
+            mc.timer.tickLength = (float) (50.0f / getTps());
+    	} else {
+            mc.timer.tickLength = 50.0f / speed.getValue();
+    	}
+    	
+
     }
 }
