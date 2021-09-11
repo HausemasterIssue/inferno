64e2a1385d01c46242844210596b05b61763e666
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-09-11 16:08:40 -0400

    gud?

--- src/main/java/me/sxmurai/inferno/features/modules/player/Timer.java
+++ src/main/java/me/sxmurai/inferno/features/modules/player/Timer.java
@@ -10,25 +10,7 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 public class Timer extends Module {
     public final Setting<Float> speed = this.register(new Setting<>("ClientTPS", 2.0f, 0.1f, 20.0f));
     public final Setting<Boolean> sync = this.register(new Setting<>("TPSSync", true));
-    
-    private static float[] ticks = new float[20];
-    
-    public static double getTps() {
-        int tickCount = 0;
-        float tickRate = 0.0f;
-
-        for (int i = 0; i < ticks.length; i++) {
-            final float tick = ticks[i];
-
-            if (tick > 0.0f) {
-                tickRate += tick;
-                tickCount++;
-            }
-        }
-
-        return MathHelper.clamp((tickRate / tickCount), 0.0f, 20.0f);
-	}
-
+   
     @Override
     protected void onActivated() {
         if (Module.fullNullCheck()) {
@@ -46,7 +28,7 @@ public class Timer extends Module {
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
     	if (sync.getValue()) {
-            mc.timer.tickLength = (float) (50.0f / getTps());
+            mc.timer.tickLength = (50.0f / Inferno.serverManager.getTps());
     	} else {
             mc.timer.tickLength = 50.0f / speed.getValue();
     	}
