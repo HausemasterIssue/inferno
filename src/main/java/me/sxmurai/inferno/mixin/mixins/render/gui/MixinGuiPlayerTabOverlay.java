4504ecb039e90dd25aa16ffc2b1daa2382a1a9dd
Sxmurai <gavrw0607@gmail.com>
2021-09-11 19:17:33 -0500

    i forgot how strict java is with their indexes oops

--- src/main/java/me/sxmurai/inferno/mixin/mixins/render/gui/MixinGuiPlayerTabOverlay.java
+++ src/main/java/me/sxmurai/inferno/mixin/mixins/render/gui/MixinGuiPlayerTabOverlay.java
@@ -13,6 +13,6 @@ import java.util.List;
 public class MixinGuiPlayerTabOverlay {
     @Redirect(method = "renderPlayerlist", at = @At(value = "INVOKE", target = "java/util/List.subList(II)Ljava/util/List;", remap = false))
     public List<NetworkPlayerInfo> hookRenderPlayerList(List<NetworkPlayerInfo> list, int from, int to) {
-        return list.subList(from, ExtraTab.INSTANCE.isToggled() ? ExtraTab.INSTANCE.players.getValue() : to);
+        return list.subList(from, Math.min(ExtraTab.INSTANCE.isToggled() ? ExtraTab.INSTANCE.players.getValue() : to, list.size()));
     }
 }
