f5cce79123fa6f71ceb9b875216eff0db6c14deb
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-09-14 20:12:28 -0400

    impossible

---
+++ src/main/java/me/sxmurai/inferno/utils/Wrapper.java
@@ -0,0 +1,32 @@
+package me.sxmurai.inferno.utils;
+
+
+import net.minecraft.client.gui.FontRenderer;
+import net.minecraft.client.multiplayer.WorldClient;
+import javax.annotation.Nullable;
+import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.client.Minecraft;
+
+public class Wrapper {
+	
+	 public static final Minecraft mc;
+	    
+	    @Nullable
+	    public static EntityPlayerSP getPlayer() {
+	        return Wrapper.mc.player;
+	    }
+	    
+	    @Nullable
+	    public static WorldClient getWorld() {
+	        return Wrapper.mc.world;
+	    }
+	    
+	    public static FontRenderer getFontRenderer() {
+	        return Wrapper.mc.fontRenderer;
+	    }
+	    
+	    static {
+	        mc = Minecraft.getMinecraft();
+	    }
+
+}
