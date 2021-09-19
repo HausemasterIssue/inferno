183a5d95e4c8f71c110ba6dca9e1bdb66f442939
Sxmurai <gavrw0607@gmail.com>
2021-09-19 12:05:59 -0500

    antilevitation

---
+++ src/main/java/me/sxmurai/inferno/features/modules/player/AntiLevitation.java
@@ -0,0 +1,16 @@
+package me.sxmurai.inferno.features.modules.player;
+
+import me.sxmurai.inferno.events.mc.UpdateEvent;
+import me.sxmurai.inferno.managers.modules.Module;
+import net.minecraft.init.MobEffects;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@Module.Define(name = "AntiLevitation", description = "", category = Module.Category.PLAYER)
+public class AntiLevitation extends Module {
+    @SubscribeEvent
+    public void onUpdate(UpdateEvent event) {
+        if (mc.player.isPotionActive(MobEffects.LEVITATION)) {
+            mc.player.removePotionEffect(MobEffects.LEVITATION);
+        }
+    }
+}
