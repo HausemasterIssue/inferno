63021fa07c3ca3391b8c75a63bdf3014150fec2f
Sxmurai <gavrw0607@gmail.com>
2021-09-19 13:46:14 -0500

    add more reverse step shit

--- src/main/java/me/sxmurai/inferno/features/modules/movement/ReverseStep.java
+++ src/main/java/me/sxmurai/inferno/features/modules/movement/ReverseStep.java
@@ -10,21 +10,35 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module.Define(name = "ReverseStep", description = "Makes you fall down faster", category = Module.Category.MOVEMENT)
 public class ReverseStep extends Module {
+    public final Setting<Double> height = this.register(new Setting<>("Height", 2.0, 0.0, 5.0));
     public final Setting<Double> speed = this.register(new Setting<>("Speed", 1.0, 0.1, 2.0));
+    public final Setting<Boolean> liquids = this.register(new Setting<>("Liquids", false));
     public final Setting<Boolean> strict = this.register(new Setting<>("Strict", false));
     public final Setting<Boolean> sneak = this.register(new Setting<>("Sneak", false));
 
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
         if (mc.player.onGround && !this.shouldStop()) {
-            // @todo strict
-            if (sneak.getValue()) {
+            if (!this.liquids.getValue() && (mc.player.isInWater() || mc.player.isInLava())) {
+                return;
+            }
+
+            if (this.sneak.getValue()) {
                 mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.START_SNEAKING));
             }
 
-            mc.player.motionY -= speed.getValue();
+            if (this.strict.getValue()) {
+                for (double y = 0.0; y < this.height.getValue() + 0.5; y += 0.01) {
+                    if (!mc.world.getCollisionBoxes(mc.player, mc.player.getEntityBoundingBox().offset(0.0, -y, 0.0)).isEmpty()) {
+                        mc.player.motionY -= this.speed.getValue();
+                        break;
+                    }
+                }
+            } else {
+                mc.player.motionY -= this.speed.getValue();
+            }
 
-            if (sneak.getValue()) {
+            if (this.sneak.getValue()) {
                 mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.STOP_SNEAKING));
             }
         }
