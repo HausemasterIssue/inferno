11d219180850ba0bfe1034d1e53eeae4b7df58b2
Sxmurai <gavrw0607@gmail.com>
2021-09-19 11:57:32 -0500

    finish autorespawn

--- src/main/java/me/sxmurai/inferno/features/modules/player/AutoRespawn.java
+++ src/main/java/me/sxmurai/inferno/features/modules/player/AutoRespawn.java
@@ -10,13 +10,18 @@ import me.sxmurai.inferno.managers.commands.text.TextBuilder;
 import me.sxmurai.inferno.managers.modules.Module;
 import me.sxmurai.inferno.utils.timing.Timer;
 import net.minecraft.client.gui.GuiGameOver;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
 
 @Module.Define(name = "AutoRespawn", description = "Does shit upon ur death", category = Module.Category.PLAYER)
 public class AutoRespawn extends Module {
     private static final FileManager FILES = FileManager.getInstance();
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy/MM/dd 'at' HH:mm:ss z");
 
     public final Setting<Boolean> noDeathScreen = this.register(new Setting<>("NoDeathScreen", true));
     public final Setting<Float> delay = this.register(new Setting<>("Delay", 0.0f, 0.0f, 20.0f, (v) -> !noDeathScreen.getValue()));
@@ -35,7 +40,7 @@ public class AutoRespawn extends Module {
 
     @SubscribeEvent
     public void onGuiScreenChange(GuiChangeEvent event) {
-        if (event.getCurrentGui() instanceof GuiGameOver && mc.player.isDead) {
+        if (event.getCurrentGui() instanceof GuiGameOver) {
             if (this.deathCoords.getValue()) {
                 if (this.chat.getValue()) {
                     Command.send(new TextBuilder()
@@ -43,17 +48,46 @@ public class AutoRespawn extends Module {
                             .append(" ")
                             .append(ChatColor.Red, "X: ")
                             .append(ChatColor.Red, String.valueOf(mc.player.posX))
-                            .append(" ")
                             .append(ChatColor.Dark_Gray, ",")
                             .append(" ")
+                            .append(ChatColor.Red, "Y: ")
+                            .append(ChatColor.Red, String.valueOf(mc.player.posY))
+                            .append(ChatColor.Dark_Gray, ",")
+                            .append(" ")
+                            .append(ChatColor.Red, "Z: ")
+                            .append(ChatColor.Red, String.valueOf(mc.player.posZ))
+                            .append(ChatColor.Dark_Gray, ".")
                     );
                 }
+
+                if (this.saveToFile.getValue()) {
+                    this.write(mc.player.getPosition());
+                }
+            }
+
+            if (this.noDeathScreen.getValue()) {
+                mc.player.respawnPlayer();
             }
         }
     }
 
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
+        if (mc.currentScreen instanceof GuiGameOver && !this.noDeathScreen.getValue() && this.timer.passedMs(this.delay.getValue().longValue())) {
+            this.timer.reset();
+            mc.player.respawnPlayer();
+        }
+    }
 
+    private void write(BlockPos pos) {
+        String ip = mc.isSingleplayer() ? "singleplayer" : Objects.requireNonNull(mc.getCurrentServerData()).serverIP;
+        Path file = this.deathCoordsFolder.resolve(ip + ".txt");
+
+        String text = FILES.readFile(file);
+        text = text == null ?
+                ("Generated " + DATE_FORMAT.format(new Date(System.currentTimeMillis())) + "\n") :
+                text;
+
+        FILES.writeFile(file, text + "\n Died at " + pos.toString() + " on " + DATE_FORMAT.format(new Date(System.currentTimeMillis())));
     }
 }
