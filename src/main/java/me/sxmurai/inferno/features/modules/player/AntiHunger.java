5119b1c26329d2a3f3cada7738f3b3a4ab7ad634
Sxmurai <gavrw0607@gmail.com>
2021-09-25 11:25:41 -0500

    updates to antihunger

--- src/main/java/me/sxmurai/inferno/features/modules/player/AntiHunger.java
+++ src/main/java/me/sxmurai/inferno/features/modules/player/AntiHunger.java
@@ -7,17 +7,28 @@ import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-@Module.Define(name = "AntiHunger", description = "Spoofs your onGround state", category = Module.Category.PLAYER)
+@Module.Define(name = "AntiHunger", description = "Attempts to stop you from loosing hunger", category = Module.Category.PLAYER)
 public class AntiHunger extends Module {
-    public final Setting<Boolean> noSprint = this.register(new Setting<>("NoSprint", false));
+    public final Setting<Boolean> ground = this.register(new Setting<>("Ground", true));
+    public final Setting<Boolean> fallCheck = this.register(new Setting<>("FallCheck", true, (v) -> ground.getValue()));
+    public final Setting<Boolean> sprint = this.register(new Setting<>("Sprint", false));
 
     @SubscribeEvent
     public void onPacketSend(PacketEvent.Send event) {
         if (!Module.fullNullCheck()) {
             if (event.getPacket() instanceof CPacketPlayer) {
-                ((CPacketPlayer) event.getPacket()).onGround = false;
-            } else if (event.getPacket() instanceof CPacketEntityAction && ((CPacketEntityAction) event.getPacket()).getAction() == CPacketEntityAction.Action.START_SPRINTING && !this.noSprint.getValue()) {
-                event.setCanceled(true);
+                if (this.ground.getValue()) {
+                    if (this.fallCheck.getValue() && mc.player.fallDistance > 3) {
+                        return;
+                    }
+
+                    ((CPacketPlayer) event.getPacket()).onGround = false;
+                }
+            } else if (event.getPacket() instanceof CPacketEntityAction) {
+                CPacketEntityAction packet = (CPacketEntityAction) event.getPacket();
+                if (packet.getAction() == CPacketEntityAction.Action.START_SPRINTING && this.sprint.getValue()) {
+                    event.setCanceled(true);
+                }
             }
         }
     }
