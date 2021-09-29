06fe6ede49b8436f08748eb6fb156c06468f755b
Sxmurai <gavrw0607@gmail.com>
2021-09-28 19:13:37 -0500

    add glintcolor

--- src/main/java/me/sxmurai/inferno/managers/modules/ModuleManager.java
+++ src/main/java/me/sxmurai/inferno/managers/modules/ModuleManager.java
@@ -77,9 +77,10 @@ public class ModuleManager {
         this.modules.add(new Yaw());
 
         // render
-        this.modules.add(new Fullbright());
         this.modules.add(new Chams());
         this.modules.add(new CrystalModifier());
+        this.modules.add(new Fullbright());
+        this.modules.add(new GlintColor());
         this.modules.add(new HandModifier());
         this.modules.add(new HoleESP());
         this.modules.add(new Nametags());
