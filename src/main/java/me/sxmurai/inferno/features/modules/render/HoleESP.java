71d6d152868a19942a9fb89f6f73839ab32c2666
Sxmurai <gavrw0607@gmail.com>
2021-10-06 19:56:07 -0500

    add range and entity checks to holeesp options

--- src/main/java/me/sxmurai/inferno/features/modules/render/HoleESP.java
+++ src/main/java/me/sxmurai/inferno/features/modules/render/HoleESP.java
@@ -5,14 +5,18 @@ import me.sxmurai.inferno.events.render.RenderEvent;
 import me.sxmurai.inferno.features.settings.Setting;
 import me.sxmurai.inferno.managers.HoleManager;
 import me.sxmurai.inferno.managers.modules.Module;
+import me.sxmurai.inferno.utils.BlockUtil;
 import me.sxmurai.inferno.utils.ColorUtils;
 import me.sxmurai.inferno.utils.RenderUtils;
 import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 @Module.Define(name = "HoleESP", description = "Shows where safe holes are", category = Module.Category.RENDER)
 public class HoleESP extends Module {
     public final Setting<VoidESP.Mode> mode = new Setting<>("Mode", VoidESP.Mode.FILLED);
+    public final Setting<Boolean> stoodIn = new Setting<>("StoodIn", true);
+    public final Setting<Float> range = new Setting<>("Range", 5.0f, 1.0f, 50.0f);
     public final Setting<Boolean> safe = new Setting<>("Safe", true);
     public final Setting<ColorUtils.Color> safeColor = new Setting<>("SafeColor", new ColorUtils.Color(0, 255, 0, 80), (v) -> safe.getValue());
     public final Setting<Boolean> unsafe = new Setting<>("Unsafe", true);
@@ -28,7 +32,17 @@ public class HoleESP extends Module {
                     continue;
                 }
 
-                AxisAlignedBB box = new AxisAlignedBB(hole.getPos());
+                BlockPos pos = hole.getPos();
+
+                if (BlockUtil.intersectsWith(pos) && !this.stoodIn.getValue()) {
+                    continue;
+                }
+
+                if (mc.player.getDistance(pos.x, pos.y, pos.z) > this.range.getValue()) {
+                    continue;
+                }
+
+                AxisAlignedBB box = new AxisAlignedBB(pos);
                 ColorUtils.Color c = hole.isSafe() ? this.safeColor.getValue() : this.unsafeColor.getValue();
                 int colour = ColorUtils.toRGBA(c.red, c.green, c.blue, c.alpha);
 
