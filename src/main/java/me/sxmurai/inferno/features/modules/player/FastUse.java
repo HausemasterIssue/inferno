4324c5d6f4fdb4c5c283bd429ea82511a0fe378a
Sxmurai <gavrw0607@gmail.com>
2021-10-06 22:09:39 -0500

    use ticktimer instead of int counter

--- src/main/java/me/sxmurai/inferno/features/modules/player/FastUse.java
+++ src/main/java/me/sxmurai/inferno/features/modules/player/FastUse.java
@@ -1,13 +1,13 @@
 package me.sxmurai.inferno.features.modules.player;
 
-import me.sxmurai.inferno.events.mc.UpdateEvent;
 import me.sxmurai.inferno.features.settings.Setting;
 import me.sxmurai.inferno.managers.modules.Module;
 import me.sxmurai.inferno.utils.InventoryUtils;
+import me.sxmurai.inferno.utils.timing.TickTimer;
 import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemBlock;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.TickEvent;
 
 @Module.Define(name = "FastUse", description = "Lets you use things fast", category = Module.Category.PLAYER)
 public class FastUse extends Module {
@@ -19,18 +19,19 @@ public class FastUse extends Module {
     public final Setting<Boolean> crystals = new Setting<>("Crystals", false);
     public final Setting<Boolean> blocks = new Setting<>("Blocks", false);
 
-    private int ticks = 0;
+    private final TickTimer timer = new TickTimer();
 
     @SubscribeEvent
-    public void onUpdate(UpdateEvent event) {
-        if (this.delay.getValue() != 0) {
-            ++this.ticks;
-            if (this.ticks <= this.delay.getValue()) {
-                return;
-            }
-
-            this.ticks = 0;
+    public void onTick(TickEvent.ClientTickEvent event) {
+        if (Module.fullNullCheck()) {
+            return;
         }
+
+        this.timer.tick();
+        if (!this.timer.passed(this.delay.getValue())) {
+            return;
+        }
+        this.timer.reset();
 
         if (this.xp.getValue() && InventoryUtils.isHolding(Items.EXPERIENCE_BOTTLE, this.offhand.getValue())) {
             mc.rightClickDelayTimer = this.speed.getValue();
