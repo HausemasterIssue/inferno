e3fcca79ea8159a2fe7ed6e7301fd99f5b5bb959
Sxmurai <gavrw0607@gmail.com>
2021-10-07 21:59:07 -0500

    remove sneak option from reversestep

--- src/main/java/me/sxmurai/inferno/features/modules/movement/ReverseStep.java
+++ src/main/java/me/sxmurai/inferno/features/modules/movement/ReverseStep.java
@@ -5,26 +5,21 @@ import me.sxmurai.inferno.events.mc.UpdateEvent;
 import me.sxmurai.inferno.features.modules.player.Freecam;
 import me.sxmurai.inferno.features.settings.Setting;
 import me.sxmurai.inferno.managers.modules.Module;
-import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+// @todo remove skidded shit
 @Module.Define(name = "ReverseStep", description = "Makes you fall down faster", category = Module.Category.MOVEMENT)
 public class ReverseStep extends Module {
     public final Setting<Double> height = new Setting<>("Height", 2.0, 0.0, 5.0);
     public final Setting<Double> speed = new Setting<>("Speed", 1.0, 0.1, 2.0);
     public final Setting<Boolean> liquids = new Setting<>("Liquids", false);
     public final Setting<Boolean> strict = new Setting<>("Strict", false);
-    public final Setting<Boolean> sneak = new Setting<>("Sneak", false);
 
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
         if (mc.player.onGround && !this.shouldStop()) {
             if (!this.liquids.getValue() && (mc.player.isInWater() || mc.player.isInLava())) {
                 return;
-            }
-
-            if (this.sneak.getValue()) {
-                mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.START_SNEAKING));
             }
 
             if (this.strict.getValue()) {
@@ -36,10 +31,6 @@ public class ReverseStep extends Module {
                 }
             } else {
                 mc.player.motionY -= this.speed.getValue();
-            }
-
-            if (this.sneak.getValue()) {
-                mc.player.connection.sendPacket(new CPacketEntityAction(mc.player, CPacketEntityAction.Action.STOP_SNEAKING));
             }
         }
     }
