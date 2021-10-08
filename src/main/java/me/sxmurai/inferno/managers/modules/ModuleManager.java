bae21de998051909d15e93533f75a60db05f5604
Sxmurai <gavrw0607@gmail.com>
2021-10-07 21:28:32 -0500

    merge VoidESP into HoleESP

--- src/main/java/me/sxmurai/inferno/managers/modules/ModuleManager.java
+++ src/main/java/me/sxmurai/inferno/managers/modules/ModuleManager.java
@@ -90,7 +90,6 @@ public class ModuleManager {
         this.modules.add(new NoRender());
         this.modules.add(new Tracers());
         this.modules.add(new ViewClip());
-        this.modules.add(new VoidESP());
         this.modules.add(new Wireframes());
         this.modules.add(new Xray());
 
