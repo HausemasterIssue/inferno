04ea43e9076c1a9a3686676125c6c1e02792399c
Sxmurai <gavrw0607@gmail.com>
2021-10-07 21:54:14 -0500

    rewrite sprint

--- src/main/java/me/sxmurai/inferno/features/modules/movement/Sprint.java
+++ src/main/java/me/sxmurai/inferno/features/modules/movement/Sprint.java
@@ -8,17 +8,15 @@ import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 @Module.Define(name = "Sprint", description = "Makes you automatically sprint", category = Module.Category.MOVEMENT)
 public class Sprint extends Module {
     public final Setting<Mode> mode = new Setting<>("Mode", Mode.LEGIT);
-    public final Setting<Boolean> hungerCheck = new Setting<>("HungerCheck", false);
-    public final Setting<Boolean> strict = new Setting<>("Strict", false);
-
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
-        if (mode.getValue() == Mode.LEGIT && mc.gameSettings.keyBindForward.pressed || mode.getValue() == Mode.RAGE) {
-            if ((hungerCheck.getValue() && mc.player.getFoodStats().getFoodLevel() <= 6) || (strict.getValue() && (mc.player.isSneaking() || mc.player.isHandActive() || mc.player.collidedHorizontally))) {
-                mc.player.setSprinting(false);
+        if (mode.getValue() == Mode.LEGIT) {
+            if (mc.player.getFoodStats().getFoodLevel() <= 6 || mc.player.isHandActive() || mc.player.isSneaking() || mc.player.collidedHorizontally || mc.player.moveForward == 0.0f) {
                 return;
             }
+        }
 
+        if (!mc.player.isSprinting()) {
             mc.player.setSprinting(true);
         }
     }
