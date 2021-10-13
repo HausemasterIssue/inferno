bfacf8bc642c7cb69387ae8640b4d2b06f55d2cd
Sxmurai <gavrw0607@gmail.com>
2021-10-12 20:14:30 -0500

    [refactor]: rewrite PingSpoof

--- src/main/java/me/sxmurai/inferno/client/features/modules/miscellaneous/PingSpoof.java
+++ src/main/java/me/sxmurai/inferno/client/features/modules/miscellaneous/PingSpoof.java
@@ -2,66 +2,51 @@ package me.sxmurai.inferno.client.features.modules.miscellaneous;
 
 import me.sxmurai.inferno.api.events.mc.UpdateEvent;
 import me.sxmurai.inferno.api.events.network.PacketEvent;
+import me.sxmurai.inferno.api.utils.timing.Timer;
 import me.sxmurai.inferno.api.values.Value;
 import me.sxmurai.inferno.client.manager.managers.modules.Module;
-import me.sxmurai.inferno.api.utils.timing.Timer;
 import net.minecraft.network.play.client.CPacketKeepAlive;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-@Module.Define(name = "PingSpoof", description = "Spoofs ur ping lol")
+@Module.Define(name = "PingSpoof", description = "Makes your ping higher, while still being able to play as if you were on lower ping")
 public class PingSpoof extends Module {
-    public final Value<Float> delay = new Value<>("Delay", 100.0f, 0.0f, 3000.0f);
+    public final Value<Double> delay = new Value<>("Delay", 0.2, 0.1, 5.0);
 
     private final Queue<CPacketKeepAlive> packets = new ConcurrentLinkedQueue<>();
-    private boolean sending = false;
     private final Timer timer = new Timer();
-
-    @Override
-    protected void onDeactivated() {
-        if (Module.fullNullCheck()) {
-            packets.clear();
-        } else {
-            emptyKeepAlives();
-        }
-
-        timer.reset();
-        sending = false;
-    }
+    private boolean pause = false;
 
     @SubscribeEvent
     public void onUpdate(UpdateEvent event) {
-        if (!Module.fullNullCheck()) {
-            if (timer.passedMs(delay.getValue().longValue())) {
-                emptyKeepAlives();
-            }
-        } else {
-            toggle();
+        if (this.timer.passedS(this.delay.getValue())) {
+            this.timer.reset();
+            this.pause = true;
+
+            this.emptyQueue();
+
+            this.pause = false;
         }
     }
 
     @SubscribeEvent
     public void onPacketSend(PacketEvent.Send event) {
-        if (!Module.fullNullCheck() && event.getPacket() instanceof CPacketKeepAlive && !sending) {
-            packets.add((CPacketKeepAlive) event.getPacket());
+        if (Module.fullNullCheck() && event.getPacket() instanceof CPacketKeepAlive && !this.pause) {
+            this.packets.add((CPacketKeepAlive) event.getPacket());
             event.setCanceled(true);
         }
     }
 
-    private void emptyKeepAlives() {
-        sending = true;
-
-        while (!packets.isEmpty()) {
-            CPacketKeepAlive packet = packets.poll();
-            if (Module.fullNullCheck() || packet == null) {
+    private void emptyQueue() {
+        while (!this.packets.isEmpty()) {
+            CPacketKeepAlive packet = this.packets.poll();
+            if (packet == null) {
                 break;
             }
 
-            mc.player.connection.getNetworkManager().dispatchPacket(packet, null);
+            mc.player.connection.sendPacket(packet);
         }
-
-        sending = false;
     }
 }
