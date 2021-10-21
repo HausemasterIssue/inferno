8156034bd47a66ae2266c12d108cfdd2e6fbe006
Sxmurai <gavrw0607@gmail.com>
2021-10-20 19:41:24 -0500

    [fix]: im a little bit of a dumbass

--- src/main/java/me/sxmurai/inferno/asm/mixins/render/MixinLayerArmorBase.java
+++ src/main/java/me/sxmurai/inferno/asm/mixins/render/MixinLayerArmorBase.java
@@ -14,14 +14,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class MixinLayerArmorBase {
     @Inject(method = "doRenderLayer", at = @At("HEAD"), cancellable = true)
     public void doRenderLayer(EntityLivingBase entitylivingbaseIn, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch, float scale, CallbackInfo info) {
-        if (NoRender.INSTANCE.isOn() && NoRender.armor.getValue() == NoRender.Armor.REMOVE) {
+        if (NoRender.INSTANCE.isOn() && NoRender.armor.getValue() == NoRender.Armor.Remove) {
             info.cancel();
         }
     }
 
     @Inject(method = "renderEnchantedGlint", at = @At("HEAD"), cancellable = true)
     private static void renderEnchantedGlint(RenderLivingBase<?> p_188364_0_, EntityLivingBase p_188364_1_, ModelBase model, float p_188364_3_, float p_188364_4_, float p_188364_5_, float p_188364_6_, float p_188364_7_, float p_188364_8_, float p_188364_9_, CallbackInfo info) {
-        if (NoRender.INSTANCE.isOn() && NoRender.armor.getValue() == NoRender.Armor.GLINT) {
+        if (NoRender.INSTANCE.isOn() && NoRender.armor.getValue() == NoRender.Armor.Remove) {
             info.cancel();
         }
     }
